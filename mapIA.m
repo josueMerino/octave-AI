8f9a43d0c01b762f2db6889073dd8a7d2c3d723f
josue.merino <josue.merino@alumnos.uneatlantico.es>
2022-10-18 16:22:06 +0200

    Cambio de archivos

--- mapIA.m
+++ mapIA.m
@@ -2,35 +2,13 @@ map = imread("mapa.jpg");
 
 dimensions = size(map)
 
-currentPosition = [1, 1];
-finalPosition = [dimensions(1), dimensions(2)];
+m = (dimensions(2)/dimensions(1));
 
-while( sum([currentPosition(1), currentPosition(2)]) < sum([finalPosition(1), finalPosition(2)]))
-  
-  finalPosition(2) = finalPosition(2) - currentPosition(2);
-  
-  finalPosition(1) = finalPosition(1) - currentPosition(1);
-  
-  differenceInX = finalPosition(1);
-  differenceInY = finalPosition(2);
-  
-  if differenceInY < differenceInX
-    if(differenceInY < 0)
-      currentPosition(2) = currentPosition(2) - 1;  
-    else
-      currentPosition(2) = currentPosition(2) + 1;
-    endif
-  elseif differenceInY >= differenceInX
-    if(differenceInX < 0)
-      currentPosition(1) = currentPosition(1) - 1;  
-    else
-      currentPosition(1) = currentPosition(1) + 1;
-    endif
-  endif
-  map(currentPosition(1),currentPosition(2), :) =[0; 153; 153];
-  
-endwhile
-%map(i, y, :) = [0; 153; 153];
+for i=1:dimensions(1)
+    y = m * i;
+    y = ceil(y);
+    map(i, y, :) = [0; 153; 153];
+endfor
 
 imshow(map)
 
--- mapIAManhattan.m
+++ mapIAManhattan.m
@@ -4,3 +4,34 @@ dimensions = size(map)
 
 currentPosition = [1, 1];
 finalPosition = [dimensions(1), dimensions(2)];
+
+while( sum([currentPosition(1), currentPosition(2)]) < sum([finalPosition(1), finalPosition(2)]))
+  
+  finalPosition(2) = finalPosition(2) - currentPosition(2);
+  
+  finalPosition(1) = finalPosition(1) - currentPosition(1);
+  
+  differenceInX = finalPosition(1);
+  differenceInY = finalPosition(2);
+  
+  if differenceInY < differenceInX
+    if(differenceInY < 0)
+      currentPosition(2) = currentPosition(2) - 1;  
+    else
+      currentPosition(2) = currentPosition(2) + 1;
+    endif
+  elseif differenceInY >= differenceInX
+    if(differenceInX < 0)
+      currentPosition(1) = currentPosition(1) - 1;  
+    else
+      currentPosition(1) = currentPosition(1) + 1;
+    endif
+  endif
+  map(currentPosition(1),currentPosition(2), :) =[0; 153; 153];
+  
+endwhile
+%map(i, y, :) = [0; 153; 153];
+
+imshow(map)
+
+disp("Programa calculo trayectoria")
